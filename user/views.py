dd621feaaf94ac8dff732c26bc0f0809c71dca14
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-13 16:50:58 -0400

    replace HttpResponse with render for each function to display respective template

--- user/views.py
+++ user/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Landing Page</h1>')
+    return render(request,'user/home.html')
 
 def user_view(request):
-    return HttpResponse('<h1>User Show Page</h1>')
+    return render(request, 'user/show.html')
 
 def user_create(request):
-    return HttpResponse('<h1>User Create Form (Registration)</h1>')
+    return render(request, 'user/new.html')
 
 def user_logout(request):
     # this should really be a session-delete function in
     # a session view that renders a confirmation page
-    return HttpResponse('<h1>User Logged Out Confirmation Page</h1>')
+    return render(request, 'user/logout.html')
