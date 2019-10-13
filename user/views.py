6a1912410a00451c0092ea8b23edf18ae921db0c
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-13 16:17:38 -0400

    add basic functions to user views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Landing Page</h1>')
+
+def user_view(request):
+    return HttpResponse('<h1>User Show Page</h1>')
+
+def user_create(request):
+    return HttpResponse('<h1>User Create Form (Registration)</h1>')
+
+def user_logout(request):
+    # this should really be a session-delete function in
+    # a session view that renders a confirmation page
+    return HttpResponse('<h1>User Logged Out Confirmation Page</h1>')
