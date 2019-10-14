f5edc5d2d54299f87c2fd33ea4ca409b92b4c712
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-14 16:11:28 -0400

    add tests for testing user creation

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UsersManagersTests(TestCase):
+
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            first_name='Zulk',
+            last_name='Ernan',
+            email='new_user@info.com',
+            password='new_user123',
+            url='https://www.myportfolio.com'
+            )
+
+        self.assertEqual(user.first_name, 'Zulk')
+        self.assertEqual(user.last_name, 'Ernan')
+        self.assertEqual(user.email, 'new_user@info.com')
+        self.assertEqual(user.password, 'new_user123')
+        self.assertEqual(user.url, 'https://www.myportfolio.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_admin)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        try:
+            self.assertIsNone(user.username)
+
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password='new123')
