83aff3d8645f965692717d88e8175979e044641f
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-14 21:06:36 -0400

    rearrange order of attributes

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import MyUser
 
-# Register your models here.
+class MyUserAdmin(UserAdmin):
+    model = MyUser
+    ordering = ('email', 'first_name', 'last_name', 'url', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+    list_display = ('email', 'first_name', 'last_name', 'url', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'url')
+    readonly_fields = ('date_joined', 'last_login', )
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(MyUser, MyUserAdmin)
