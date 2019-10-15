815ed983961869d21223cbc8bd2bdf9e205bf0cd
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-15 01:50:31 -0400

    add login form for authenticating user

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 
 from users.models import MyUser
 
@@ -10,3 +11,20 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = MyUser
         fields = ('first_name', 'last_name', 'email', 'password1', 'password2', 'url')
+
+
+# Create a custom form for authentication/login
+# Meta specifies user model & fields of the form
+# clean displays validation error if the fields are invalid
+class MyUserAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    class Meta:
+        model = MyUser
+        fields = ('email', 'password')
+
+    def clean(self):
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+
+        if not authenticate(email=email, password=password):
+            raise forms.ValidationError("Invalid login")
