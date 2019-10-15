a8ac7a5a391ecf888ec500e6ce3e956a25917afa
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-15 00:23:57 -0400

    add urls for logout

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('register', views.registration_view, name='registration')
+    path('register', views.registration_view, name='registration'),
+    path('logout', views.logout_view, name='logout'),
 ]
