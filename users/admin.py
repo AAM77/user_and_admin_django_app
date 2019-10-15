6791cee92a7e55cab74ec8a1c0c7e57b9ec29d2b
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-14 21:10:46 -0400

    Revert "rearrange order of attributes"
    This reverts commit 83aff3d8645f965692717d88e8175979e044641f.

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from users.models import MyUser
 
-class MyUserAdmin(UserAdmin):
-    model = MyUser
-    ordering = ('email', 'first_name', 'last_name', 'url', 'date_joined', 'last_login', 'is_admin', 'is_staff')
-    list_display = ('email', 'first_name', 'last_name', 'url', 'date_joined', 'last_login', 'is_admin', 'is_staff')
-    search_fields = ('email', 'first_name', 'last_name', 'url')
-    readonly_fields = ('date_joined', 'last_login', )
-
-    filter_horizontal = ()
-    list_filter = ()
-    fieldsets = ()
-
-
-admin.site.register(MyUser, MyUserAdmin)
+# Register your models here.
