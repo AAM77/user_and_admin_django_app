2cf5ee9b8c1bf15ec55ecd949291772ae33a1f08
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-14 23:20:43 -0400

    add a basic registration view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from users.forms import RegistrationForm
 
 
 def home_view(request):
     return render(request, 'users/home.html')
+
+def registration_view(request):
+    context = {}
+    return render(request, 'users/register.html', context)
