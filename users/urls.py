7df836f5a3b4ece966eeb87978b997a1c6614818
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-16 05:21:45 -0400

    add path to urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import UserListView, UserDetailView
+from .views import (
+    UserListView,
+    UserDetailView,
+    UserCreateView
+)
 from . import views
 
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('user/new/', UserCreateView.as_view(), name='user-create'),
+
 ]
