24d8290d77cdbbafc790d3681170f177a58ecef8
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-16 05:27:17 -0400

    fix path

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,6 @@ from . import views
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('user/new/', UserCreateView.as_view(), name='user-create'),
+    path('new/', UserCreateView.as_view(), name='user-create'),
 
 ]
