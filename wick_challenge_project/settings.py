7c164a56c9caaa8b562b9b36069f35e93a0bc0a0
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-15 22:51:19 -0400

    change database to MySQL as per requirements.

--- wick_challenge_project/settings.py
+++ wick_challenge_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'wick_challenge_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wickchallenge',
+        'USER': 'root',
+        'PASSWORD': '12345abcde',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
